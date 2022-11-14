e038c70b4f3ba7657d4183ef3d25586eeb5efb28
averrunci <averrunci@fievus.com>
2022-11-14 10:52:02 +0900

    Add the ProgressScope class.

---
+++ Source/Charites.Bindings/ProgressScope.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (C) 2022 Fievus
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+namespace Charites.Windows.Mvc.Bindings;
+
+/// <summary>
+/// Represents a scope in which the specified indicator is set to true until this instance is disposed.
+/// </summary>
+public sealed class ProgressScope : IDisposable
+{
+    private readonly ObservableProperty<bool> progressIndicator;
+
+    private ProgressScope(ObservableProperty<bool> progressIndicator)
+    {
+        this.progressIndicator = progressIndicator;
+        this.progressIndicator.Value = true;
+    }
+
+    /// <summary>
+    /// Creates a scope in which the specified indicator is set to true until this instance is disposed.
+    /// </summary>
+    /// <param name="progressIndicator">The indicator that is set to true in a scope.</param>
+    /// <returns>A new instance of the <see cref="ProgressScope"/> class.</returns>
+    public static ProgressScope By(ObservableProperty<bool> progressIndicator) => new(progressIndicator);
+
+    /// <summary>
+    /// Sets the indicator to false.
+    /// </summary>
+    public void Dispose()
+    {
+        progressIndicator.Value = false;
+    }
+}
---
+++ Spec/Charites.Bindings.Spec/ProgressScopeSpec.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (C) 2022 Fievus
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+using Carna;
+
+namespace Charites.Windows.Mvc.Bindings;
+
+[Specification("ProgressScope Spec")]
+class ProgressScopeSpec : FixtureSteppable
+{
+    ObservableProperty<bool> ProgressIndicator { get; } = false.ToObservableProperty();
+
+    ProgressScope Scope { get; set; } = default!;
+
+    [Example("Begins/ends a scope")]
+    void Ex01()
+    {
+        Expect("the initial indicator value should be false", () => !ProgressIndicator.Value);
+
+        When("to begin a scope by specifying the indicator", () => Scope = ProgressScope.By(ProgressIndicator));
+        Then("the indicator value should be true", () => ProgressIndicator.Value);
+        When("to end the scope", () => Scope.Dispose());
+        Then("the indicator value should be false", () => !ProgressIndicator.Value);
+    }
+}
